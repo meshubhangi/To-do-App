157a0e7faaaa7f6fef3291c96d37cb9c466d250f
Shubhangi Pandey <shubhangi.pandey08@gmail.com>
2018-07-10 23:01:42 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,20 @@
+var addButton = document.getElementById("add");
+var taskInput = document.getElementById("task");
+var taskList = document.getElementById("taskList");
+
+addButton.addEventListener("click", function(){
+    var task = taskInput.value;
+    
+    if(task.trim()){
+
+        var newItem = document.createElement("LI");
+        var taskText = document.createTextNode(task);
+        newItem.appendChild(taskText);
+        
+        taskInput.value = "";
+        taskList.appendChild(newItem);
+    }
+    else{
+        alert("Task cannot be empty");
+    }
+});
